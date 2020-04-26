d39e4250de61107391f0006449e0604f95414713
Manderson1995 <30336122+Manderson1995@users.noreply.github.com>
2020-04-26 12:17:31 -0400

    Added ProfileComponenet

--- Angular/Marcus-Portfolio/src/app/app.module.ts
+++ Angular/Marcus-Portfolio/src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
+import { ProfileComponent } from './profile/profile.component';
+import { ProfileModule } from './profile/profile.module';
+import { HttpClientModule } from '@angular/common/http';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: ProfileComponent
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
+    RouterModule.forRoot(routes, {useHash: true}),
+    ProfileModule,
     AppRoutingModule,
     HttpClientModule
   ],
