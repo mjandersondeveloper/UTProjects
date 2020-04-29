7e3b8e93e06dedc463086f158693c9a83f13a927
Manderson1995 <30336122+Manderson1995@users.noreply.github.com>
2020-04-28 23:05:16 -0400

    Added header/nav items

--- Angular/Marcus-Portfolio/src/app/profile/profile.module.ts
+++ Angular/Marcus-Portfolio/src/app/profile/profile.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProfileComponent,
+    HeaderComponent
+  ],
   imports: [
     CommonModule
   ]
