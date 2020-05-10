2374481dbbffbd0bff1a7bc4d7fa267ea69e5677
Manderson1995 <30336122+Manderson1995@users.noreply.github.com>
2020-05-09 20:07:58 -0400

    add profile mods

--- Angular/Marcus-Portfolio/src/app/profile/profile.module.ts
+++ Angular/Marcus-Portfolio/src/app/profile/profile.module.ts
@@ -2,13 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { ProfileComponent } from './profile.component';
+import { AboutComponent } from './about/about.component';
+import { IntroComponent } from './intro/intro.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { GithubComponent } from './github/github.component';
+import { SkillsComponent } from './skills/skills.component';
+import { EducationComponent } from './education/education.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 
 @NgModule({
   declarations: [
     ProfileComponent,
-    HeaderComponent
+    HeaderComponent,
+    AboutComponent,
+    IntroComponent,
+    ExperienceComponent,
+    GithubComponent,
+    SkillsComponent,
+    EducationComponent,
+    ContactComponent
   ],
   imports: [
     CommonModule
